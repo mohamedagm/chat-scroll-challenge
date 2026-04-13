b8a7cfc55672cda74e5ec038ef1cac19039725e7
mohamedagm <abogm44ud@gmail.com>
2026-04-13 07:01:41 +0200

    Built web
    Branch: main
    Commit: adf95f8b5bc4bda5c68b239b94a2e78eb773c249 (dirty)
    
    package:peanut 6.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6b5f892db92dde9ea8c491a048ac811f
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "245306e6a6d88b7292a93da43c1028e7",
+"flutter_bootstrap.js": "6527476fa93ed5155adc415d3d81e686",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
